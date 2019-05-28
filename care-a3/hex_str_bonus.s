3bfeb1c132ae569ffc50177f95fef107c2d0e740
johnnyd <johnnyd@asb9840u-e06.csil.sfu.ca>
2019-05-27 17:06:37 -0700

    Remove duplicate

--- care-a3/hex_str_bonus.s
+++
@@ -1,5 +0,0 @@
-	.globl hex_str
-hex_str:
-	movw $'?', (%rsi)   #  remove this line and write your code here
-
-	ret
--- care-a4/min.c
+++
@@ -1,8 +0,0 @@
-
-int min(int, int);
-
-int min(int a, int b) {
-    if (a < b) return a;
-    return b;
-}
-
